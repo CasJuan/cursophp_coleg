7b4581231cf87283681bd09fa2eee7e2a28761df
CasJuan <castrojuanig25@gmail.com>
2025-01-08 15:37:12 -0300

    [cleanCode] abstaccion

---
+++ clean-code/09-abstraccio.php
@@ -0,0 +1,46 @@
+<?php
+
+class Tokens {
+    function tokenizer(){
+        $tokens =[];
+        foreach ($tokens as $token) {
+            //verificamos
+        }
+        return $tokens;
+    }
+}
+
+
+class Users {
+    function tokenizer( array $jwt){
+        $users = $jwt;
+        foreach ($users as $user) {
+            //asignamos tokens al usuario
+        }
+        return $user;
+    }
+}
+
+
+class JWT{
+    private $tokens;
+    private $users;
+
+    public function __construct(Tokens $tokens, Users $users)
+    {   
+        $this->tokens = $tokens;
+        $this->users = $users;
+    }
+
+    function read(array $jwt){
+        $tokens = $this->tokens->tokenizer();
+        $users = $this->users->tokenizer($jwt);
+
+        $jwToArrays = [];
+        foreach ($jwToArrays as $jwToArrays) {
+            //convertimos jwt a array php
+        }
+
+    }
+}
+
