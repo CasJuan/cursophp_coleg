e76d9157d1aada0ffdf8b4c0c331beeba63d09f9
CasJuan <castrojuanig25@gmail.com>
2025-01-08 13:26:58 -0300

    [cleanCode] simplificar la informacion sin miedo usar foreach

---
+++ clean-code/05-mapas-mentale.php
@@ -0,0 +1,12 @@
+<?php
+
+$countries = ["mexico","argentina","eeuu","madrid"];
+
+/* for ($i=0; $i < count($c) ; $i++) { 
+    echo "City $c[$i]";
+}; */
+
+foreach ($countries as $conuntry) {
+    echo "City $conuntry";
+}
+
