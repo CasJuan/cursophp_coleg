3088593267c67fd3c67979a0d640509b8f3963c7
CasJuan <castrojuanig25@gmail.com>
2025-01-08 12:56:28 -0300

    [cleanCode] constantes ayudar como un diccionario

--- clean-code/02-vocabulario.php
+++ clean-code/02-vocabulario.php
@@ -1,15 +1,23 @@
 <?php
 
 class User {
+
+    //Diccionario de Roles
+
+    const ADMIN_ROL = 1;
+    const SECURITY_ROL = 2;
+    const STORE_ROL = 3;
     
     private $id;
     private $name;
     private $age;
+    private $rol;
     
     public function _constructor($id, $name, $age){
         $this->id = $id;
         $this->name = $name;
         $this->age = $age;
+        $this->rol = 1;
     }
     
     public function getID(){
@@ -23,10 +31,15 @@ class User {
     public function userAge(){
         return $this->age;
     }
+    
+    public function getRol(){
+        return $this->rol;
+    }
 }
 
 $user = new User(10,"Juan",30);
 
-echo "User ID: {$user -> getID()} Nombre: {$user -> getName()} Age: {$user -> userAge()}";
+/* echo "User ID: {$user -> getID()} Nombre: {$user -> getName()} Age: {$user -> userAge()}"; */
+echo $user -> getRol() == User::ADMIN_ROL ? "Admin" : "Otro";
 
 
