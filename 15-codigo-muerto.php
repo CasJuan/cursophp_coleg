d4b51bbba95c5fd8a80870e523a20688b4a25ae6
CasJuan <castrojuanig25@gmail.com>
2025-01-09 11:29:10 -0300

    [celanCode] agrego descpripciones que ayudan a la funcion debajo

--- 15-codigo-muerto.php
+++ 15-codigo-muerto.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * Esta funcion envia por correos lo siguient:
+ * 
+ * 1.informe
+ * 2.mensaje
+ * 
+ * Ademas recibe lo siguiente
+ * $body = "<h1>Hola mundo</h1>"
+ * 
+ */
+
+
 function sendMail () {
     
 }
