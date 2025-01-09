4548b1959996ef7a980f2ec5bf6bfa7cd0a5605e
CasJuan <castrojuanig25@gmail.com>
2025-01-09 11:35:14 -0300

    [celanCode] agrego mas descripciones como params o deprecated

--- 15-codigo-muerto.php
+++ 15-codigo-muerto.php
@@ -1,6 +1,8 @@
 <?php
 
 /**
+ * @deprecated quedo sustituida en la version 3.1.1
+ * 
  * Esta funcion envia por correos lo siguient:
  * 
  * 1.informe
@@ -8,6 +10,14 @@
  * 
  * Ademas recibe lo siguiente
  * $body = "<h1>Hola mundo</h1>"
+ * 
+ * @link https://informacio.com";
+ * @since 1.2
+ * 
+ * @param array $users a quienes vamos enviar el mail
+ * @param string $body cuerpo del correo 
+ * 
+ * @return void no retorna nada
  * 
  */
 
